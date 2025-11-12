f56e60e32d4936b0de7fb175182b7a92eed4ce8f
Chiman2937 <cyga2933@gmail.com>
2025-11-12 12:08:14 +0900

    feat: utils.ts 추가
    - cn function 추가: twMerge + clsx

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+import { ClassValue } from 'clsx';
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
