eff577993cc10661cc1ce84ed8710de578c5eebf
Chiman2937 <cyga2933@gmail.com>
2025-11-12 19:27:58 +0900

    feat: framer motion 패키지 추가
    - Lazy Loading 적용(최적화)

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,4 +1,5 @@
 'use client';
+import LazyMotionProvider from '@/providers/LazyMotionProvider';
 import { MSWProvider } from '@/providers/MSWProvider';
 import { QueryProvider } from '@/providers/QueryProvider';
 
@@ -9,7 +10,9 @@ interface Props {
 export const Providers = ({ children }: Props) => {
   return (
     <QueryProvider>
-      <MSWProvider>{children}</MSWProvider>
+      <MSWProvider>
+        <LazyMotionProvider>{children}</LazyMotionProvider>
+      </MSWProvider>
     </QueryProvider>
   );
 };
---
+++ src/providers/LazyMotionProvider.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+// use
+// import * as m from "motion/react-m";
+// instead of
+// import { motion } from "motion/react";
+
+import { LazyMotion } from 'motion/react';
+
+const loadFeatures = () => import('@/lib/feature').then((res) => res.default);
+
+export default function LazyMotionProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <LazyMotion features={loadFeatures} strict>
+      {children}
+    </LazyMotion>
+  );
+}
