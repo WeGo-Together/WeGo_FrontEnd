b55ca08588c93008328004e101c335fc88a1c021
Chiman2937 <cyga2933@gmail.com>
2025-11-20 08:56:24 +0900

    fix: commit 시 commitlint config에서 지정한 type만 허용하도록 수정

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -5,5 +5,8 @@ module.exports = {
     'subject-min-length': [2, 'always', 5],
     'subject-max-length': [2, 'always', 100],
     'body-max-line-length': [2, 'always', 100], // body 한 줄 최대 100글자
+    'type-case': [0],
+    'type-empty': [2, 'never'],
+    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'chore', 'remove', 'style']],
   },
 };
