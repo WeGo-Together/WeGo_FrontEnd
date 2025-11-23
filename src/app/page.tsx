e00b4716a36cf00c0a0b7f65a653ca5ed3dc88db
Chiman2937 <cyga2933@gmail.com>
2025-11-23 19:00:33 +0900

    chore: 빌드되는 커밋

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 const HomePage = async () => {
-  const res = await fetch('https://invalid-url-that-does-not-exist-12345.com/api');
-  const data = await res.json();
-
-  return <div>{data.content}첫번째 PR</div>;
+  return <div>첫번째 PR</div>;
 };
 
 export default HomePage;
