bd411234d266c45aa901a01e248cd1d468990afb
HopeFullee <john_lee0607@naver.com>
2025-12-04 16:11:29 +0900

    fix: 모임상세 더보기 400px 이하에서 고장현상 수리

--- src/components/pages/meetup/meetup-members/index.tsx
+++ src/components/pages/meetup/meetup-members/index.tsx
@@ -17,47 +17,63 @@ interface Props {
 
 export const MeetupMembers = ({ members }: Props) => {
   const [showMore, setShowMore] = useState(false);
+  const [coverMember, setCoverMember] = useState(2 < Math.ceil(members.length / 3));
 
-  const floorCount = Math.ceil(11 / 3);
-  const showShowMoreButton = 2 < floorCount;
+  const hasMoreMember = 2 < Math.ceil(members.length / 3);
 
   const onShowMoreClick = () => {
     setShowMore((prev) => !prev);
+    setCoverMember((prev) => !prev);
   };
 
   return (
     <section className='relative bg-gray-50 px-4 pt-6 pb-4'>
       <h3 className='text-text-md-semibold px-2 text-gray-800'>참여자 정보</h3>
 
-      <div
-        className={clsx(
-          'mt-5 overflow-y-hidden transition-all',
-          floorCount === 1 ? 'h-25' : 'h-50',
-          showMore ? `h-[${100 * floorCount}px]` : 'h-50',
-        )}
-      >
-        <ul className='flex flex-wrap'>
-          {members.map(({ name, profileImage }) => (
-            <li key={name} className='h-25 w-34 shrink-0'>
-              <Link href={'#'} className='flex-col-center gap-[6px]'>
-                <Image
-                  width={64}
-                  className='h-16 w-16 rounded-full'
-                  alt='프로필 사진'
-                  height={64}
-                  objectFit='cover'
-                  src={profileImage}
-                />
-                <p className='text-text-xs-medium line-clamp-1 w-full text-center break-all text-gray-800'>
+      <div className='mt-5'>
+        <ul className='flex-center flex-wrap'>
+          {members.map(({ name, profileImage }, idx) => (
+            <li
+              key={name}
+              className={clsx(
+                'relative h-25 w-34 shrink-0',
+                hasMoreMember && !showMore ? '[&:nth-child(n+7)]:hidden' : 'block',
+              )}
+            >
+              <div className='flex-col-center gap-[6px]'>
+                <Link href={'#'} className='select-none'>
+                  <Image
+                    width={64}
+                    className='h-16 w-16 rounded-full'
+                    alt='프로필 사진'
+                    height={64}
+                    objectFit='cover'
+                    src={profileImage}
+                  />
+                </Link>
+                <p
+                  className={clsx(
+                    'text-text-xs-medium line-clamp-1 w-full text-center break-all text-gray-800',
+                    coverMember && idx === 5 ? 'hidden' : 'block',
+                  )}
+                >
                   {name}
                 </p>
-              </Link>
+              </div>
+
+              {coverMember && idx === 5 && (
+                <div className='absolute inset-0'>
+                  <span className='flex-center text-text-md-semibold mx-auto h-[65px] w-[65px] rounded-full bg-gray-200 text-gray-600'>
+                    {members.length - 5}+
+                  </span>
+                </div>
+              )}
             </li>
           ))}
         </ul>
       </div>
 
-      {showShowMoreButton && (
+      {hasMoreMember && (
         <div className='flex-center'>
           <Button
             className='flex-center h-9 w-auto border-none bg-gray-50 px-4'
