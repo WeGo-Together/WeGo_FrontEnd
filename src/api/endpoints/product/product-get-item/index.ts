658b9896478e28e37a5537c6864216380726df5c
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2025-12-04 20:35:00 +0900

    Update src/api/endpoints/product/product-get-item/index.ts

--- src/api/endpoints/product/product-get-item/index.ts
+++ src/api/endpoints/product/product-get-item/index.ts
@@ -4,7 +4,7 @@ import { GetProductItemResponse } from '@/types/endpoints/products/types';
 // 상품 상세 조회 기본 API - OK
 export const getProduct = async (id: number) => {
   const response = await httpClient<GetProductItemResponse>(`/products/${id}`, {
-    method: 'POST',
+    method: 'GET',
   });
   return response;
 };
