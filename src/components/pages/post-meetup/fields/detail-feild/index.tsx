988ee275e7ba541a3f4685aa1b434b1aafb002b2
HopeFullee <john_lee0607@naver.com>
2025-12-05 17:02:23 +0900

    style: 모임생성 페이지 기본 마크업 작업 (폼연동X)

---
+++ src/components/pages/post-meetup/fields/detail-feild/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Label } from '@/components/ui';
+
+export const MeetupDetailField = () => {
+  return (
+    <div className='mt-3 flex w-full flex-col gap-1'>
+      <Label htmlFor='post-meetup-detail' required>
+        모임 날짜
+      </Label>
+      <textarea
+        id='post-meetup-detail'
+        className='bg-mono-white focus:border-mint-500 text-text-md-medium h-40 w-full resize-none rounded-2xl border border-gray-300 px-5 py-4 text-gray-800 focus:outline-none'
+        maxLength={300}
+        placeholder='모임에 대해 설명해주세요'
+        required
+      />
+      <div className='text-text-sm-medium text-right text-gray-500'>0/300</div>
+    </div>
+  );
+};
---
+++ src/components/pages/post-meetup/index.ts
@@ -0,0 +1,7 @@
+export { MeetupAddressField } from './fields/address-field';
+export { MeetupCapField } from './fields/cap-field';
+export { MeetupDateField } from './fields/date-field';
+export { MeetupDetailField } from './fields/detail-feild';
+export { MeetupTagsField } from './fields/tags-field';
+export { MeetupTitleField } from './fields/title-field';
+export { MeetupSubmitButton } from './post-button';
