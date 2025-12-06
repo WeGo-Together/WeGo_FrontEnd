c599784819914c8d0df3dbac518eaabf0deba1c9
HopeFullee <john_lee0607@naver.com>
2025-12-07 02:14:16 +0900

    feat: 모임생성 tanstack-form 폼 연동

--- src/components/pages/post-meetup/index.ts
+++ src/components/pages/post-meetup/index.ts
@@ -3,5 +3,6 @@ export { MeetupCapField } from './fields/cap-field';
 export { MeetupDateField } from './fields/date-field';
 export { MeetupDetailField } from './fields/detail-feild';
 export { MeetupTagsField } from './fields/tags-field';
+export { MeetupThumbnailsField } from './fields/thumbnails-field';
 export { MeetupTitleField } from './fields/title-field';
 export { MeetupSubmitButton } from './post-button';
