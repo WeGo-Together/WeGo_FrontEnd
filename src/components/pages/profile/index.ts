293227bc5115abc0f1a01f5ec38584cbc1f7fe28
Chiman2937 <cyga2933@gmail.com>
2025-12-08 12:53:53 +0900

    feat: 프로필,마이페이지 용 컴포넌트 제작

---
+++ src/components/pages/profile/index.ts
@@ -0,0 +1,7 @@
+export { ProfileCard } from './profile-card';
+export { ProfileDescription } from './profile-description';
+export { ProfileDescriptionBadge } from './profile-description-badge';
+export { ProfileFollowsBadge } from './profile-follows-badge';
+export { ProfileInfo } from './profile-info';
+export { ProfileSetting } from './profile-setting';
+export { ProfileToggleButton as ProfileSettingButton } from './profile-setting/profile-setting-button';
---
+++ src/components/pages/profile/profile-card/index.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+
+import { User } from '@/types/service/user';
+
+interface Props {
+  user: User;
+}
+
+export const ProfileCard = ({ user }: Props) => {
+  const { profileImage, nickName, profileMessage } = user;
+  return (
+    <div className='flex-col-center mb-6'>
+      <div className='relative mb-3 size-24 overflow-hidden rounded-full'>
+        <Image alt='image' fill objectFit='cover' src={profileImage} />
+      </div>
+      <h2 className='text-text-xl-bold text-gray-900'>{nickName}</h2>
+      <p className='text-text-sm-medium text-gray-600'>{profileMessage}</p>
+    </div>
+  );
+};
