6868df18af7810c91480c86d57eb0e27c9552730
Chiman2937 <cyga2933@gmail.com>
2025-12-08 13:10:54 +0900

    fix: ProfileSettingButton export 삭제

--- src/components/pages/profile/index.ts
+++ src/components/pages/profile/index.ts
@@ -4,4 +4,3 @@ export { ProfileDescriptionBadge } from './profile-description-badge';
 export { ProfileFollowsBadge } from './profile-follows-badge';
 export { ProfileInfo } from './profile-info';
 export { ProfileSetting } from './profile-setting';
-export { ProfileToggleButton as ProfileSettingButton } from './profile-setting/profile-setting-button';
