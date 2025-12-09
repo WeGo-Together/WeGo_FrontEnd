03190d16859677c1f5c77e38e0595ab78cb6df86
Chiman2937 <cyga2933@gmail.com>
2025-12-09 15:14:40 +0900

    feat: 공통 응답 생성 함수 제작

--- src/mock/service/user/users-handler.ts
+++ src/mock/service/user/users-handler.ts
@@ -2,37 +2,43 @@ import { http, HttpResponse } from 'msw';
 
 import { User } from '@/types/service/user';
 
+import { createErrorResponse, createSuccessResponse } from '../common/common-mock';
 import { mockUserItems } from './users-mock';
 
-const getUserItemMock = http.get(`*/api/v1/users/:userId`, ({ params }) => {
+const getUserItemMock = http.get(`*/users/:userId`, ({ params }) => {
   const id = Number(params.userId);
   const user = mockUserItems.find((item) => item.id === id);
 
   if (!user) {
-    return HttpResponse.json({ message: '존재하지 않는 유저입니다' }, { status: 404 });
+    return HttpResponse.json(
+      createErrorResponse({ status: 404, detail: '존재하지 않는 유저입니다.', errorCode: 'U001' }),
+      { status: 404 },
+    );
   }
 
-  return HttpResponse.json(user);
+  return HttpResponse.json(createSuccessResponse(user));
 });
 
-const updateUserItemMock = http.patch(`*/api/v1/users`, async ({ request }) => {
+const updateUserItemMock = http.patch(`*/users`, async ({ request }) => {
   const body = (await request.json()) as User;
-  return HttpResponse.json({
-    ...mockUserItems[0],
-    ...body,
-  });
+  return HttpResponse.json(
+    createSuccessResponse({
+      ...mockUserItems[0],
+      ...body,
+    }),
+  );
 });
 
-const deleteUserItemMock = http.delete(`*/api/v1/users`, async () => {
-  return new HttpResponse(null, { status: 204 });
+const deleteUserItemMock = http.delete(`*/users`, async () => {
+  return HttpResponse.json(createSuccessResponse(null));
 });
 
-const followUserItemMock = http.post(`*/api/v1/follows`, async () => {
-  return new HttpResponse(null, { status: 204 });
+const followUserItemMock = http.post(`*/follows`, async () => {
+  return HttpResponse.json(createSuccessResponse(null));
 });
 
-const unfollowUserItemMock = http.delete(`*/api/v1/follows/:followId`, async () => {
-  return new HttpResponse(null, { status: 204 });
+const unfollowUserItemMock = http.delete(`*/follows/:followId`, async () => {
+  return HttpResponse.json(createSuccessResponse(null));
 });
 
 export const userHandlers = [
