af284ec938cf3981d1eab91a35a3415e0e344cf6
Chiman2937 <cyga2933@gmail.com>
2025-12-09 23:27:05 +0900

    Revert "chore: 스토리 변경 테스트"
    This reverts commit 236eb0fac6367ba5be51332bf4ae0219b11342bd.

--- src/components/ui/button/index.stories.tsx
+++ src/components/ui/button/index.stories.tsx
@@ -71,3 +71,12 @@ export const Small: Story = {
     size: 'sm',
   },
 };
+
+export const Disabled: Story = {
+  args: {
+    children: '사용불가',
+    variant: 'primary',
+    size: 'md',
+    disabled: true,
+  },
+};
