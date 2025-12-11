8d169a2c246c01c2c07a45c0217c5a133c4eb007
minseokim <claudia503@naver.com>
2025-12-11 11:51:00 +0900

    feat: 이미지 추가

--- src/app/schedule/(components)/my.tsx
+++ src/app/schedule/(components)/my.tsx
@@ -7,7 +7,9 @@ const MOCK_MEETINGS: Meeting[] = [
   {
     id: 2,
     title: '주말 러닝 크루 모집',
-    images: [],
+    images: [
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?q=80&w=717&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ],
     location: '한강공원 잠실지구',
     dateTime: '25. 12. 05 - 07:30',
     nickName: 'Minseo Kim',
