7e1aa24ac6cbe25a08a422429079adb7ec66a0fd
Kim Chiyoung <cyga2933@gmail.com>
2025-12-12 15:59:59 +0900

    fix: 마이페이지 CSR 적용

--- src/app/(user)/mypage/page.tsx
+++ src/app/(user)/mypage/page.tsx
@@ -1,12 +1,27 @@
 'use client';
+import { useEffect, useState } from 'react';
+
+import Cookies from 'js-cookie';
+
 import { MyPageInfo, MyPageSetting } from '@/components/pages/user/mypage';
 import { useGetUser } from '@/hooks/use-user';
 
 const MyPage = () => {
-  const userId = 1;
+  // const [userId, setUserId] = useState(0);
+  const [userId, setUserId] = useState(0);
   // 여기서 user 정보를 확인해서 undefined이면 로그인페이지로 리다이렉트
 
-  const { data: user } = useGetUser({ userId });
+  const { data: user } = useGetUser({ userId }, { enabled: !!userId });
+
+  // userId가 MyPage의 파라미터로 전달되지 않기 때문에 직접 cookie로 꺼내와야함
+  // 하지만 서버에서는 js-cookie 활용 불가능 => hydration Error 발생
+  // 따라서 userId를 state롤 관리하고 useEffect 시 userId를 쿠키에 불러와야 해결가능
+  useEffect(() => {
+    const id = Cookies.get('userId');
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setUserId(Number(id));
+  }, []);
+
   if (!user) return null;
   return (
     <div className='flex min-h-[calc(100dvh-113px)] flex-col justify-between'>
