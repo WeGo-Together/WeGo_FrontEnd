20054b7b9032ba1db21c8ade5084551141cdf86a
yoolri <lucca6042@gmail.com>
2025-12-12 11:14:28 +0900

    feat: auth 관련 hook 생성
    - 로그인, 로그아웃, 회원가입, 회원탈퇴

---
+++ src/hooks/use-auth/use-auth-logout/index.ts
@@ -0,0 +1,30 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { useQueryClient } from '@tanstack/react-query';
+
+import { API } from '@/api';
+import { userKeys } from '@/lib/query-key/query-key-user';
+
+export const useLogout = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const handleLogout = async () => {
+    try {
+      await API.authService.logout();
+    } catch (error) {
+      console.error('[LOGOUT ERROR]', error);
+    } finally {
+      // 로그인 유저 관련 캐시 정리
+      queryClient.removeQueries({ queryKey: userKeys.all });
+
+      // 로컬 스토리지/추가 상태도 정리???
+
+      router.push('/');
+    }
+  };
+
+  return handleLogout;
+};
