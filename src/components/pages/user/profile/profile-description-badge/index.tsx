02c9f8225b9fec9703a91222df006c8129d76f66
Chiman2937 <cyga2933@gmail.com>
2025-12-14 10:00:33 +0900

    fix: profileDescriptionBadge - MBTI value가 없을 때도 공간 차지하도록 수정

--- src/components/pages/user/profile/profile-description-badge/index.tsx
+++ src/components/pages/user/profile/profile-description-badge/index.tsx
@@ -1,4 +1,5 @@
 import { Icon, IconId } from '@/components/icon';
+import { cn } from '@/lib/utils';
 
 export interface ProfileDescriptionBadgeProps {
   label: string;
@@ -11,6 +12,8 @@ interface Props {
 }
 
 export const ProfileDescriptionBadge = ({ badgeItems }: Props) => {
+  const PLACEHOLDER = '-';
+
   return (
     <div className='flex flex-row items-center gap-4'>
       <div className='flex-center size-10 rounded-xl bg-gray-100'>
@@ -18,7 +21,14 @@ export const ProfileDescriptionBadge = ({ badgeItems }: Props) => {
       </div>
       <div className='flex flex-col'>
         <span className='text-text-xs-medium text-gray-500'>{badgeItems.label}</span>
-        <span className='text-text-md-semibold text-gray-700'>{badgeItems.value}</span>
+        <span
+          className={cn(
+            'text-text-md-semibold h-6 text-gray-700',
+            !badgeItems.value && 'text-text-md-regular text-gray-500',
+          )}
+        >
+          {badgeItems.value || PLACEHOLDER}
+        </span>
       </div>
     </div>
   );
