9eec781788584a4a9ed261fc5487075da06b9a3f
minseokim <claudia503@naver.com>
2025-12-15 13:49:23 +0900

    feat: 카드 썸네일 ImageWithFallback 적용

--- src/components/shared/card/card-thumbnail/index.tsx
+++ src/components/shared/card/card-thumbnail/index.tsx
@@ -1,26 +1,25 @@
-import Image from 'next/image';
+import { DEFAULT_GROUP_IMAGE } from 'constants/default-images';
+
+import { ImageWithFallback } from '@/components/ui';
 
 type CardThumbnailProps = {
   title: string;
   thumbnail?: string;
   hasThumbnail: boolean;
-  onError: () => void;
 };
 
-export const CardThumbnail = ({ title, thumbnail, hasThumbnail, onError }: CardThumbnailProps) => {
+export const CardThumbnail = ({ title, thumbnail, hasThumbnail }: CardThumbnailProps) => {
   return (
     <div className='relative h-[100px] w-[100px] shrink-0 overflow-hidden rounded-2xl bg-gray-200'>
-      {hasThumbnail && thumbnail ? (
-        <Image
-          width={100}
-          className='h-full w-full object-cover'
-          alt={title}
-          height={100}
-          src={thumbnail}
-          unoptimized
-          onError={onError}
-        />
-      ) : null}
+      <ImageWithFallback
+        width={100}
+        className='h-full w-full object-cover'
+        alt={title}
+        fallbackSrc={DEFAULT_GROUP_IMAGE}
+        height={100}
+        src={hasThumbnail && thumbnail ? thumbnail : ''}
+        unoptimized
+      />
     </div>
   );
 };
