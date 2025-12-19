111e3c5b79a9c16571bd82a9b52ee88abaddd0df
wooktori <jjun9503@gmail.com>
2025-12-19 17:35:39 +0900

    feat: follow search 컴포넌트 test 작성

--- src/components/pages/message/message-following-search/index.test.tsx
+++ src/components/pages/message/message-following-search/index.test.tsx
@@ -2,8 +2,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
 import { ModalProvider } from '@/components/ui';
+import { useAddFollowers } from '@/hooks/use-follower';
 
 import { FollowingSearch } from '.';
+
+jest.mock('@/hooks/use-follower', () => ({
+  useAddFollowers: jest.fn(),
+}));
 
 const createQueryClient = () =>
   new QueryClient({
@@ -18,8 +23,8 @@ const createQueryClient = () =>
   });
 
 describe('Following Search 테스트', () => {
-  const queryClient = createQueryClient();
-  test('Following Search 렌더링 테스트', () => {
+  let queryClient: QueryClient;
+  const renderForm = () =>
     render(
       <QueryClientProvider client={queryClient}>
         <ModalProvider>
@@ -27,18 +32,25 @@ describe('Following Search 테스트', () => {
         </ModalProvider>
       </QueryClientProvider>,
     );
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    (useAddFollowers as jest.Mock).mockReturnValue({
+      mutate: jest.fn(),
+    });
+
+    queryClient = createQueryClient();
+  });
+
+  test('Following Search 렌더링 테스트', () => {
+    renderForm();
 
     expect(screen.getByText('팔로우 추가')).toBeInTheDocument();
   });
 
   test('팔로우 추가 클릭 시 모달 생성', () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        <ModalProvider>
-          <FollowingSearch userId={0} />
-        </ModalProvider>
-      </QueryClientProvider>,
-    );
+    renderForm();
 
     expect(screen.queryByText('팔로우 할 닉네임을 입력하세요')).toBeNull();
 
@@ -48,13 +60,7 @@ describe('Following Search 테스트', () => {
   });
 
   test('공백 상태로 팔로우 버튼 클릭 시 에러 메시지 렌더링 테스트.', () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        <ModalProvider>
-          <FollowingSearch userId={0} />
-        </ModalProvider>
-      </QueryClientProvider>,
-    );
+    renderForm();
 
     fireEvent.click(screen.getByText('팔로우 추가'));
 
@@ -64,14 +70,58 @@ describe('Following Search 테스트', () => {
     expect(screen.getByText('닉네임을 입력해주세요.')).toBeInTheDocument();
   });
 
+  test('팔로우 추가 성공 테스트', async () => {
+    (useAddFollowers as jest.Mock).mockReturnValue({
+      mutate: jest.fn((_data, options) => {
+        options?.onSuccess?.();
+      }),
+    });
+
+    renderForm();
+
+    // 모달 열기
+    fireEvent.click(screen.getByText('팔로우 추가'));
+
+    // 닉네임 입력
+    fireEvent.change(screen.getByPlaceholderText('nickname'), {
+      target: { value: 'HelloWorld' },
+    });
+
+    // 팔로우 버튼 클릭
+    fireEvent.click(screen.getByRole('button', { name: '팔로우' }));
+
+    // 모달 닫힘 테스트
+    await waitFor(() => {
+      expect(screen.queryByText('팔로우 할 닉네임을 입력하세요')).toBeNull();
+    });
+  });
+
+  test('팔로우 추가 실패 테스트', async () => {
+    (useAddFollowers as jest.Mock).mockReturnValue({
+      mutate: jest.fn((_data, options) => {
+        options?.onError?.();
+      }),
+    });
+
+    renderForm();
+
+    // 모달 열기
+    fireEvent.click(screen.getByText('팔로우 추가'));
+
+    expect(screen.getByText('팔로우 할 닉네임을 입력하세요')).toBeInTheDocument();
+
+    // 잘못된 닉네임 입력
+    fireEvent.change(screen.getByPlaceholderText('nickname'), { target: { value: '중복닉네임' } });
+
+    // 팔로우 버튼 클릭
+    fireEvent.click(screen.getByText('팔로우'));
+
+    // 에러 div 체크
+    expect(screen.getByText('존재하지 않는 유저입니다.'));
+  });
+
   test('취소 버튼 클릭 시 모달 닫힘', async () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        <ModalProvider>
-          <FollowingSearch userId={0} />
-        </ModalProvider>
-      </QueryClientProvider>,
-    );
+    renderForm();
     fireEvent.click(screen.getByText('팔로우 추가'));
 
     expect(screen.getByText('팔로우 할 닉네임을 입력하세요')).toBeInTheDocument();
@@ -81,6 +131,66 @@ describe('Following Search 테스트', () => {
 
     await waitFor(() => {
       expect(screen.queryByText('팔로우 할 닉네임을 입력하세요')).not.toBeInTheDocument();
+    });
+  });
+
+  test('에러 메세지가 떠 있는 상태에서 input 변경 시 에러 메세지 사라짐', () => {
+    (useAddFollowers as jest.Mock).mockReturnValue({
+      mutate: jest.fn((_data, options) => {
+        options?.onError?.();
+      }),
+    });
+
+    renderForm();
+
+    fireEvent.click(screen.getByText('팔로우 추가'));
+
+    expect(screen.getByText('팔로우 할 닉네임을 입력하세요')).toBeInTheDocument();
+
+    fireEvent.change(screen.getByPlaceholderText('nickname'), {
+      target: { value: '잘못된 닉네임' },
+    });
+
+    fireEvent.click(screen.getByText('팔로우'));
+
+    expect(screen.getByText('존재하지 않는 유저입니다.'));
+
+    fireEvent.change(screen.getByPlaceholderText('nickname'), {
+      target: { value: '잘못된 닉네임ㅋ' },
+    });
+
+    expect(screen.queryByText('존재하지 않는 유저입니다.')).toBeNull();
+  });
+
+  test('input에서 Enter 키 입력 시 팔로우가 실행', async () => {
+    (useAddFollowers as jest.Mock).mockReturnValue({
+      mutate: jest.fn((_data, options) => {
+        options?.onSuccess?.();
+      }),
+    });
+
+    renderForm();
+
+    // 모달 열기
+    fireEvent.click(screen.getByText('팔로우 추가'));
+
+    const input = screen.getByRole('textbox');
+
+    // 닉네임 입력
+    fireEvent.change(input, {
+      target: { value: 'HelloWorld' },
+    });
+
+    // Enter 키 입력
+    fireEvent.keyDown(input, {
+      key: 'Enter',
+      code: 'Enter',
+      charCode: 13,
+    });
+
+    // 성공 시 모달 닫힘
+    await waitFor(() => {
+      expect(screen.queryByText('팔로우 할 닉네임을 입력하세요')).toBeNull();
     });
   });
 });
