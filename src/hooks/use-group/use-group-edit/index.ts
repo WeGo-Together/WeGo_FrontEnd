c8ac979946d83a4fbaffd328f848912aa287e0be
HopeFullee <john_lee0607@naver.com>
2025-12-23 18:07:29 +0900

    feat: 모임수정 API hook 작업

---
+++ src/hooks/use-group/use-group-edit/index.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { API } from '@/api';
+import { groupKeys } from '@/lib/query-key/query-key-group';
+import { CreateGroupPayload, GroupIdParams } from '@/types/service/group';
+
+export const useEditGroup = (params: GroupIdParams) => {
+  const queryClient = useQueryClient();
+
+  const query = useMutation({
+    mutationFn: (payload: CreateGroupPayload) => API.groupService.editGroup(params, payload),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: groupKeys.detail(params.groupId) });
+
+      console.log('모임 생성 성공.');
+    },
+    onError: () => {
+      console.log('모임 생성 실패.');
+    },
+  });
+  return query;
+};
