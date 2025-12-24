72950133da92fd6e6a731e579bc6be81196f93c8
Chiman2937 <cyga2933@gmail.com>
2025-12-24 21:10:45 +0900

    feat: 빈 화면 UI 제작

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+import { EmptyState } from '@/components/layout/empty-state';
 import { NotificationCard, NotificationHeader } from '@/components/pages/notification';
 import { useGetNotificationsInfinite } from '@/hooks/use-notification/use-notification-get-list';
 
 export default function NotificationPage() {
-  const { data: notificationList, fetchNextPage } = useGetNotificationsInfinite({ size: 1 });
+  const { data: list, fetchNextPage } = useGetNotificationsInfinite({ size: 1 });
 
-  if (!notificationList) return;
+  if (!list) return;
 
   return (
     <section>
@@ -15,9 +16,9 @@ export default function NotificationPage() {
         <p className='text-mono-white bg-mint-500 flex-center size-4 rounded-full'>v</p>
         <p className='text-mono-black text-text-sm mr-3 text-right'>모두 읽음 처리</p>
       </div>
-      {notificationList.map((item, idx) => (
-        <NotificationCard key={idx} item={item} />
-      ))}
+
+      {list.length > 0 && list.map((item, idx) => <NotificationCard key={idx} item={item} />)}
+      {list.length === 0 && <EmptyState>아직 받은 알림이 없어요.</EmptyState>}
       <button className='text-black' onClick={() => fetchNextPage()}>
         다음
       </button>
