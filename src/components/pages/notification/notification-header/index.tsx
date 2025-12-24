c01b83583432015cef2c34da125de58a5e682e51
Chiman2937 <cyga2933@gmail.com>
2025-12-25 08:33:37 +0900

    fix: Notification Header - 뒤로가기 버튼을 button 요소로 수정

--- src/components/pages/notification/notification-header/index.tsx
+++ src/components/pages/notification/notification-header/index.tsx
@@ -7,19 +7,18 @@ export const NotificationHeader = () => {
   const router = useRouter();
 
   const handleHistoryBackClick = () => {
-    console.log('뒤로가라');
     router.back();
   };
 
   return (
     <nav className='bg-mono-white flex-center sticky top-14 z-10 h-12 border-b-1 border-gray-200'>
-      <Icon
-        id='chevron-left-2'
-        className='absolute left-5 size-6 cursor-pointer rounded-md text-gray-500 transition-colors duration-300 hover:bg-gray-100 active:bg-gray-100'
+      <button
+        className='absolute left-5 size-6 cursor-pointer rounded-md transition-colors duration-300 hover:bg-gray-100 active:bg-gray-100'
         aria-label='뒤로 가기'
-        role='button'
         onClick={handleHistoryBackClick}
-      />
+      >
+        <Icon id='chevron-left-2' className='text-gray-500' />
+      </button>
       <h2 className='text-text-md-bold text-gray-800'>알림</h2>
     </nav>
   );
