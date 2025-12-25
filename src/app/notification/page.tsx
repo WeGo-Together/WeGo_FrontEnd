96e2b9af21ea9a9313ff6d8d8aa8dff363224ea4
Chiman2937 <cyga2933@gmail.com>
2025-12-25 09:09:44 +0900

    fix: NotificationCard - key를 index 값에서 item.id 값으로 수정

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -17,7 +17,7 @@ export default function NotificationPage() {
         <p className='text-mono-black text-text-sm mr-3 text-right'>모두 읽음 처리</p>
       </div>
 
-      {list.length > 0 && list.map((item, idx) => <NotificationCard key={idx} item={item} />)}
+      {list.length > 0 && list.map((item) => <NotificationCard key={item.id} item={item} />)}
       {list.length === 0 && <EmptyState>아직 받은 알림이 없어요.</EmptyState>}
       <button className='text-black' onClick={() => fetchNextPage()}>
         다음
