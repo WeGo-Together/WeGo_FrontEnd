f7f498372233cbe1914df1999710cbe9823072e5
wooktori <jjun9503@gmail.com>
2025-12-29 17:42:42 +0900

    fix: 테스트 코드 수정

--- src/app/message/page.test.tsx
+++ src/app/message/page.test.tsx
@@ -1,9 +1,9 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen } from '@testing-library/react';
 
+import { FollowingContent } from '@/components/pages/message/message-following-content';
 import { ModalProvider } from '@/components/ui';
 import { useInfiniteScroll } from '@/hooks/use-group/use-group-infinite-list';
-
-import FollowingPage from './page';
 
 jest.mock('@/hooks/use-group/use-group-infinite-list', () => ({
   useInfiniteScroll: jest.fn(),
@@ -15,12 +15,18 @@ jest.mock('next/navigation', () => ({
   }),
 }));
 
-jest.mock('js-cookie', () => ({
-  get: () => '1',
-}));
-
 describe('FollowingPage 테스트', () => {
+  let queryClient: QueryClient;
+
   beforeEach(() => {
+    queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+
     (useInfiniteScroll as jest.Mock).mockReturnValue({
       items: [],
       error: null,
@@ -31,11 +37,17 @@ describe('FollowingPage 테스트', () => {
     });
   });
 
+  afterEach(() => {
+    queryClient.clear();
+  });
+
   test('팔로잉이 없을 경우 FollowingNone을 보여준다', async () => {
     render(
-      <ModalProvider>
-        <FollowingPage />
-      </ModalProvider>,
+      <QueryClientProvider client={queryClient}>
+        <ModalProvider>
+          <FollowingContent initialUserId={1} />
+        </ModalProvider>
+      </QueryClientProvider>,
     );
 
     expect(await screen.findByText('아직 팔로우 한 사람이 없어요.')).toBeInTheDocument();
