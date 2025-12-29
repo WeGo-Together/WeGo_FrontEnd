ddcf88c4d0498000e87beb0e85d1f93da51cbd88
Chiman2937 <cyga2933@gmail.com>
2025-12-29 14:43:27 +0900

    fix: 알림 페이지 무한 스크롤 기능 추가

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -2,26 +2,33 @@
 
 import { EmptyState } from '@/components/layout/empty-state';
 import { NotificationCard, NotificationHeader } from '@/components/pages/notification';
+import { useIntersectionObserver } from '@/hooks/use-intersection-observer';
 import { useGetNotificationsInfinite } from '@/hooks/use-notification/use-notification-get-list';
 
 export default function NotificationPage() {
-  const { data: list, fetchNextPage } = useGetNotificationsInfinite({ size: 1 });
+  const {
+    data: list,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useGetNotificationsInfinite({ size: 20 });
+
+  const fetchObserverRef = useIntersectionObserver({
+    onIntersect: () => {
+      if (hasNextPage && !isFetchingNextPage) {
+        fetchNextPage();
+      }
+    },
+  });
 
   if (!list) return;
 
   return (
     <section>
       <NotificationHeader />
-      <div className='flex h-10 flex-row items-center justify-end gap-2'>
-        <p className='text-mono-white bg-mint-500 flex-center size-4 rounded-full'>v</p>
-        <p className='text-mono-black text-text-sm mr-3 text-right'>모두 읽음 처리</p>
-      </div>
-
       {list.length > 0 && list.map((item) => <NotificationCard key={item.id} item={item} />)}
+      {hasNextPage && <div ref={fetchObserverRef}>다음</div>}
       {list.length === 0 && <EmptyState>아직 받은 알림이 없어요.</EmptyState>}
-      <button className='text-black' onClick={() => fetchNextPage()}>
-        다음
-      </button>
     </section>
   );
 }
