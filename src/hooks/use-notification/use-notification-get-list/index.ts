9e42a09b6defe8b401340e7f907909c36b7bf82e
Chiman2937 <cyga2933@gmail.com>
2025-12-29 14:37:29 +0900

    fix: 알림 목록 queryKey에 params 객체 삭제

--- src/hooks/use-notification/use-notification-get-list/index.ts
+++ src/hooks/use-notification/use-notification-get-list/index.ts
@@ -6,7 +6,7 @@ import { GetNotificationListQueryParams } from '@/types/service/notification';
 
 export const useGetNotificationsInfinite = (params?: GetNotificationListQueryParams) => {
   return useInfiniteQuery({
-    queryKey: notificationKeys.list(params),
+    queryKey: notificationKeys.list(),
     queryFn: ({ pageParam }) => API.notificationService.getList({ ...params, cursor: pageParam }),
     initialPageParam: params?.cursor,
     getNextPageParam: (lastPage) => lastPage.nextCursor,
--- src/lib/query-key/query-key-notification/index.ts
+++ src/lib/query-key/query-key-notification/index.ts
@@ -1,11 +1,5 @@
-import { GetNotificationListQueryParams } from '@/types/service/notification';
-
 export const notificationKeys = {
   all: ['notifications'] as const,
-  list: (params?: GetNotificationListQueryParams) => [
-    'notifications',
-    'list',
-    ...(params ? [params] : []),
-  ],
+  list: () => ['notifications', 'list'],
   unReadCount: () => [...notificationKeys.all, 'unread-count'],
 };
