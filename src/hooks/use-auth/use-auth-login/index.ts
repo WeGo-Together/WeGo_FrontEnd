071ba91d7eb5039192b52ee07df14d4d65e17e84
yoolri <lucca6042@gmail.com>
2025-12-31 17:12:57 +0900

    refactor: 코드 정리

--- src/hooks/use-auth/use-auth-login/index.ts
+++ src/hooks/use-auth/use-auth-login/index.ts
@@ -5,7 +5,6 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { useCallback, useState } from 'react';
 
 import axios, { AxiosError } from 'axios';
-import Cookies from 'js-cookie';
 
 import { API } from '@/api';
 import { normalizePath } from '@/lib/auth/utils';
@@ -26,17 +25,17 @@ const getLoginErrorMessage = (problem: CommonErrorResponse) => {
 };
 
 // 📜 proxy 설정 후 삭제
-const isCommonErrorResponse = (e: unknown): e is CommonErrorResponse => {
-  if (!e || typeof e !== 'object') return false;
+// const isCommonErrorResponse = (e: unknown): e is CommonErrorResponse => {
+//   if (!e || typeof e !== 'object') return false;
 
-  const obj = e as Record<string, unknown>;
-  return (
-    typeof obj.status === 'number' &&
-    typeof obj.detail === 'string' &&
-    typeof obj.errorCode === 'string' &&
-    typeof obj.instance === 'string'
-  );
-};
+//   const obj = e as Record<string, unknown>;
+//   return (
+//     typeof obj.status === 'number' &&
+//     typeof obj.detail === 'string' &&
+//     typeof obj.errorCode === 'string' &&
+//     typeof obj.instance === 'string'
+//   );
+// };
 
 export const useLogin = () => {
   const router = useRouter();
@@ -51,15 +50,7 @@ export const useLogin = () => {
     setLoginError(null);
 
     try {
-      const result = await API.authService.login(payload);
-      // 📜 추후 삭제
-      console.log('login success:', result);
-
-      Cookies.set('userId', String(result.user.userId), {
-        path: '/',
-        sameSite: 'lax',
-        secure: process.env.NODE_ENV === 'production',
-      });
+      await API.authService.login(payload);
 
       formApi.reset();
 
@@ -68,11 +59,11 @@ export const useLogin = () => {
       const nextPath = normalizePath(searchParams.get('path'));
       router.replace(nextPath);
     } catch (error) {
-      if (isCommonErrorResponse(error)) {
-        console.error('[LOGIN ERROR]', error.errorCode, error.detail);
-        setLoginError(getLoginErrorMessage(error));
-        return;
-      }
+      // if (isCommonErrorResponse(error)) {
+      //   console.error('[LOGIN ERROR]', error.errorCode, error.detail);
+      //   setLoginError(getLoginErrorMessage(error));
+      //   return;
+      // }
 
       if (axios.isAxiosError(error)) {
         const axiosError = error as AxiosError<CommonErrorResponse>;
