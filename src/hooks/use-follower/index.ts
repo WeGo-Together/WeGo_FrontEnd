c9c6472242d68ed0466ee107bdc432725b7891c2
Chiman2937 <cyga2933@gmail.com>
2026-01-06 11:41:44 +0900

    fix: 빌드에러 수정

--- src/hooks/use-follower/index.ts
+++ src/hooks/use-follower/index.ts
@@ -1,4 +1,3 @@
-export { useGetFollowees } from './use-followee-get';
 export { useGetFolloweesInfinite } from './use-followee-list-get';
 export { useAddFollowers } from './use-follower-add';
 export { useGetFollowers } from './use-follower-get';
