a2077c7a6d905b7b037f1b42ecb66d45d18778aa
HopeFullee <john_lee0607@naver.com>
2026-01-10 22:37:44 +0900

    fix: 모임상세 useQuery hook을 useSuspense로 변경

--- src/hooks/use-group/use-group-get-details/index.ts
+++ src/hooks/use-group/use-group-get-details/index.ts
@@ -1,11 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { API } from '@/api';
 import { groupKeys } from '@/lib/query-key/query-key-group';
 import { GroupIdParams } from '@/types/service/group';
 
 export const useGetGroupDetails = (params: GroupIdParams) => {
-  const query = useQuery({
+  const query = useSuspenseQuery({
     queryKey: groupKeys.detail(params.groupId),
     queryFn: () => API.groupService.getGroupDetails(params),
   });
