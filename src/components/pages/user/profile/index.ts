06379b2f3394d3c22d072b2166f53dee58ea2fad
Chiman2937 <cyga2933@gmail.com>
2026-01-11 21:06:52 +0900

    refactor: 프로필,마이페이지 구조 리팩토링

--- src/components/pages/user/profile/index.ts
+++ src/components/pages/user/profile/index.ts
@@ -1,6 +1,5 @@
 export { ProfileCard } from './profile-card';
 export { ProfileDescription } from './profile-description';
-export { ProfileDescriptionBadge } from './profile-description-badge';
 export { ProfileEditModal } from './profile-edit-modal';
 export { ProfileFollowsBadge } from './profile-follows-badge';
 export { ProfileFollowsModal } from './profile-follows-modal';
