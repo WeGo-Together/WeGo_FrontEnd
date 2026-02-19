cecc82e3d65a3d67d40cc7f0b87cbc2f992d1ab3
Chiman2937 <cyga2933@gmail.com>
2026-02-19 22:40:45 +0900

    fix: 공통응답타입 수정
    - CommonSuccessResponse: 일반 interface로 수정
    - CommonErrorResponse: contructor 구문 수정

--- src/types/service/common.ts
+++ src/types/service/common.ts
@@ -1,18 +1,23 @@
 export class CommonErrorResponse {
-  constructor(
-    public type: string,
-    public title: string,
-    public status: number,
-    public detail: string,
-    public instance: string,
-    public errorCode?: string,
-  ) {}
+  type: string;
+  title: string;
+  status: number;
+  detail: string;
+  instance: string;
+  errorCode: string;
+
+  constructor(data?: Partial<CommonErrorResponse>) {
+    this.type = data?.type ?? 'about:blank';
+    this.title = data?.title ?? 'Network Error';
+    this.status = data?.status ?? 0;
+    this.detail = data?.detail ?? '서버와 연결할 수 없습니다.';
+    this.instance = data?.instance ?? '';
+    this.errorCode = data?.errorCode ?? 'NETWORK_ERROR';
+  }
 }
 
-export class CommonSuccessResponse<T> {
-  constructor(
-    public status: number,
-    public success: boolean,
-    public data: T,
-  ) {}
+export interface CommonSuccessResponse<T> {
+  status: number;
+  success: boolean;
+  data: T;
 }
