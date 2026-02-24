6d34e3c01cbc9871e58a290e871a7c377b5132c4
Chiman2937 <cyga2933@gmail.com>
2026-02-24 14:33:01 +0900

    fix: NotificationStore - 인터페이스 명 수정

--- src/stores/useNotificationStore/index.ts
+++ src/stores/useNotificationStore/index.ts
@@ -2,14 +2,14 @@ import { create } from 'zustand';
 
 import { NotificationItem } from '@/types/service/notification';
 
-interface AuthState {
+interface NotificationState {
   receivedData: NotificationItem | null;
   setReceivedData: (value: NotificationItem | null) => void;
   hasNewNotification: boolean;
   setHasNewNotification: (value: boolean) => void;
 }
 
-export const useNotificationStore = create<AuthState>((set) => ({
+export const useNotificationStore = create<NotificationState>((set) => ({
   receivedData: null,
   setReceivedData: (value) => set({ receivedData: value }),
   hasNewNotification: false,
